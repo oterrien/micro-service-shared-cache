a14bd390a8215c293c10c6a5519f4797f874354a
oterrien <oterrien@neuf.fr>
2017-05-26 19:35:48 +0200

    update

--- client-scheduler/src/main/java/com/ote/test/ClientBatchApplication.java
+++ client-scheduler/src/main/java/com/ote/test/ClientBatchApplication.java
@@ -3,7 +3,9 @@ package com.ote.test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -14,5 +16,8 @@ public class ClientBatchApplication {
         new SpringApplicationBuilder(ClientBatchApplication.class).run(args);
     }
 
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- client-scheduler/src/main/java/com/ote/test/Scheduler.java
+++ client-scheduler/src/main/java/com/ote/test/Scheduler.java
@@ -1,17 +1,23 @@
 package com.ote.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
-@Component
+@Controller
+@Slf4j
 public class Scheduler {
 
     @Autowired
     private ProxyService proxyService;
 
     @Scheduled(fixedRate = 5000)
-    public void callProxy(){
-        proxyService.read(2);
+    private void callProxy() {
+        try {
+            log.info(proxyService.read(2).toString());
+        } catch (Exception e){
+            log.error(e.getMessage());
+        }
     }
 }
